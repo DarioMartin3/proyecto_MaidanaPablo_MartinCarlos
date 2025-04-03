ea4cdde54053edc79b115f7d3e1480c3afb1c883
Dario <dariomartin3@gmail.com>
2025-04-03 11:31:48 -0300

    feat(layout): add layout

---
+++ app/Views/layout.php
@@ -0,0 +1,151 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>404 Clothing</title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="assets/css/mystyle.css" rel="stylesheet" />
+  </head>
+  <body>
+    <!--Barra de navegacion-->
+    <section class="container-fluid px-0">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <!--Logo en la barra-->
+          <a class="navbar-brand" href="#">
+            <img
+              src="./assets/img/logo2.png"
+              alt="404 Clothing"
+              width="150"
+              height="100"
+            />
+          </a>
+          <!--<a class="navbar-brand" href="#">404 Clothing</a>-->
+          <!--Boton para moviles-->
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <!--Boton Categoria-->
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Categorias
+                </a>
+                <!--Contenido del menu desplegable-->
+                <ul class="dropdown-menu">
+                  <li class="dropdown-columns">
+                    <ul class="column">
+                      <li>
+                        <a class="dropdown-item highlight-item" href="#">Parte de Arriba</a>
+                      </li>
+                      <li><a class="dropdown-item" href="#">Remeras</a></li>
+                      <li><a class="dropdown-item" href="#">Musculosas</a></li>
+                      <li><a class="dropdown-item" href="#">Camisas</a></li>
+                    </ul>
+                    <ul class="column">
+                      <li>
+                        <a class="dropdown-item highlight-item" href="#">Parte de Abajo</a>
+                      </li>
+                      <li><a class="dropdown-item" href="#">Pantalons</a></li>
+                      <li><a class="dropdown-item" href="#">Bermudas</a></li>
+                      <li><a class="dropdown-item" href="#">Zapatillas</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+              <!--Boton Coleeccion del momento-->
+              <li class="nav-item">
+                <a class="nav-link" href="#">Colección </a>
+              </li>
+              <!--Boton Outlet-->
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Outlet
+                </a>
+                <!--Contenido del menu desplegable-->
+                <ul class="dropdown-menu">
+                  <li class="dropdown-columns">
+                    <ul class="column">                      
+                      <li><a class="dropdown-item" href="#">Remeras</a></li>
+                      <li><a class="dropdown-item" href="#">Musculosas</a></li>
+                      <li><a class="dropdown-item" href="#">Camisas</a></li>                    
+                      <li><a class="dropdown-item" href="#">Pantalones</a></li>
+                      <li><a class="dropdown-item" href="#">Bermudas</a></li>
+                      <li><a class="dropdown-item" href="#">Zapatillas</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+            <!--Buscador-->
+            <form class="d-flex" role="search">
+              <input
+                class="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button class="btn btn-dark" type="submit">
+                Search
+              </button>                            
+            </form>
+          </div>
+        </div>
+      </nav>
+    </section>
+    <!--Carrusel automatico-->
+    <section class="container mt-4">
+      <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="./assets/img/carrusel1.jpg" class="d-block w-100" alt="New Coleccion Woman" loading="lazy">
+          </div>
+          <div class="carousel-item">
+            <img src="./assets/img/carrusel2.jpg" class="d-block w-100" alt="New Coleccion Men" loading="lazy">
+          </div>
+          <div class="carousel-item">
+            <img src="./assets/img/carrusel3.jpg" class="d-block w-100" alt="Outlet" loading="lazy">
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div> 
+    </section>
+    <?php echo $this->renderSection('content'); ?>
+    <!--Footer-->
+    <script src="assets/js/bootstrap.bundle.min.js" ></script>
+  </body>
+</html>
