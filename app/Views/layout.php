32b9c65bb9e43235e00524c75ab436cd369ecf36
Dario <dariomartin3@gmail.com>
2025-04-03 11:39:20 -0300

    fix(homepage): carousel

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -115,35 +115,7 @@
         </div>
       </nav>
     </section>
-    <!--Carrusel automatico-->
-    <section class="container mt-4">
-      <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
-        <div class="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="./assets/img/carrusel1.jpg" class="d-block w-100" alt="New Coleccion Woman" loading="lazy">
-          </div>
-          <div class="carousel-item">
-            <img src="./assets/img/carrusel2.jpg" class="d-block w-100" alt="New Coleccion Men" loading="lazy">
-          </div>
-          <div class="carousel-item">
-            <img src="./assets/img/carrusel3.jpg" class="d-block w-100" alt="Outlet" loading="lazy">
-          </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
-      </div> 
-    </section>
+    
     <?php echo $this->renderSection('content'); ?>
     <!--Footer-->
     <script src="assets/js/bootstrap.bundle.min.js" ></script>
