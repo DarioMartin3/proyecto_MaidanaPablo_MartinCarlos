b53f589c268e9788641e8ab7a6795a0a92685bab
Dario <dariomartin3@gmail.com>
2025-04-26 19:22:44 -0300

    feat: update homep and styles cards

--- app/Views/front/homep.php
+++ app/Views/front/homep.php
@@ -1,146 +1,148 @@
-    <!--Carrusel automatico-->
-    <section class="container-fluid px-0 mt-4">
-      <div
-        id="carouselExampleAutoplaying"
-        class="carousel slide"
-        data-bs-ride="carousel"
-        data-bs-interval="3000">        
-        <div class="carousel-indicators">
-          <button
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide-to="0"
-            class="active"
-            aria-current="true"
-            aria-label="Slide 1"></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide-to="1"
-            aria-label="Slide 2"></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide-to="2"
-            aria-label="Slide 3"></button>
-        </div>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img
-              src="./assets/img/carrusel/foto1.jpg"
-              class="d-block w-100"
-              alt="New Coleccion Woman"
-              loading="lazy" />
-          </div>
-          <div class="carousel-item">
-            <img
-              src="./assets/img/carrusel/foto2.jpg"
-              class="d-block w-100"
-              alt="New Coleccion Men"
-              loading="lazy" />
-          </div>
-          <div class="carousel-item">
-            <img
-              src="./assets/img/carrusel/foto3.jpg"
-              class="d-block w-100"
-              alt="Outlet"
-              loading="lazy" />
-          </div>
-        </div>
-        <button
-          class="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleAutoplaying"
-          data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button
-          class="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleAutoplaying"
-          data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
+<!--Carrusel automatico-->
+<section class="container-fluid px-0 mt-4">
+  <div
+    id="carouselExampleAutoplaying"
+    class="carousel slide"
+    data-bs-ride="carousel"
+    data-bs-interval="3000">
+    <div class="carousel-indicators">
+      <button
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide-to="0"
+        class="active"
+        aria-current="true"
+        aria-label="Slide 1"></button>
+      <button
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide-to="1"
+        aria-label="Slide 2"></button>
+      <button
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide-to="2"
+        aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img
+          src="./assets/img/carrusel/foto1.jpg"
+          class="d-block w-100"
+          alt="New Coleccion Woman"
+          loading="lazy" />
       </div>
-    </section>    
-    <section class="conteiner-fluid">
-      <div class="d-grid gap-3 colum_responsive">
-        <div class="p-3 "> <a href="#">
-            <div class="card text-bg-dark float borde-card">
-              <img src="./assets/img/imagenes_cartila/ropa_mujer.jpg" class="card-img" alt="Ropa mujer">
-              <div class="card-img-overlay">
-                <h5 class="card-title">Ropa mujer</h5>
-              </div>
-            </div>
-          </a></div>
-        <div class="p-3"> <a href="#">
-            <div class="card text-bg-dark float borde-card">
-              <img src="./assets/img/imagenes_cartila/ropa_hombre.jpg" class="card-img" alt="Ropa Hombre">
-              <div class="card-img-overlay">
-                <h5 class="card-title">Ropa Hombre</h5>
-              </div>
-            </div>
-          </a></div>
-        <div class="p-3"> <a href="#">
-            <div class="card text-bg-dark float borde-card">
-              <img src="./assets/img/imagenes_cartila/ropa_invierno.jpg" class="card-img" alt="Ropa de Invierno">
-              <div class="card-img-overlay">
-                <h5 class="card-title">Ropa de Invierno</h5>
-              </div>
-            </div>
-          </a></div>
+      <div class="carousel-item">
+        <img
+          src="./assets/img/carrusel/foto2.jpg"
+          class="d-block w-100"
+          alt="New Coleccion Men"
+          loading="lazy" />
+      </div>
+      <div class="carousel-item">
+        <img
+          src="./assets/img/carrusel/foto3.jpg"
+          class="d-block w-100"
+          alt="Outlet"
+          loading="lazy" />
+      </div>
+    </div>
+    <button
+      class="carousel-control-prev"
+      type="button"
+      data-bs-target="#carouselExampleAutoplaying"
+      data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button
+      class="carousel-control-next"
+      type="button"
+      data-bs-target="#carouselExampleAutoplaying"
+      data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+</section>
+<section class="conteiner-fluid">
+  <div class="d-grid gap-3 colum_responsive">
+    <div class="p-3 "> <a href="#">
+        <div class="card text-bg-dark float borde-card">
+          <img src="./assets/img/imagenes_cartila/ropa_mujer.jpg" class="card-img" alt="Ropa mujer">
+          <div class="card-img-overlay">
+            <span class="span-overlay">Ropa Mujer</span>
+          </div>
 
-      </div>
-    </section class="conteiner-fluid">
-    <section>
-      <div class="d-grid gap-3">
-        <div class="p-3 ">
-          <a href="#">
-            <div class="card text-bg-dark float borde-card">
-              <img src="./assets/img/imagenes_cartila/ver_todo.jpg" class="card-img" alt="Ver Todo">
-              <div class="card-img-overlay">
-                <h5 class="card-title">Ver Todo</h5>
-              </div>
-            </div>
-          </a>
         </div>
-    </section>
-    
-    <section class="conteiner-fluid">
-      <h1>Algunos productos</h1>
-      <div class="d-grid gap-3 colum_responsive">
-          <div class="p-3">
-            <div class="card text-bg-dark float borde-card" >
-              <img src="./assets/img/imagenes_cartila/ropa_mujer.jpg" class="card-img" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
+      </a></div>
+    <div class="p-3"> <a href="#">
+        <div class="card text-bg-dark float borde-card">
+          <img src="./assets/img/imagenes_cartila/ropa_hombre.jpg" class="card-img" alt="Ropa Hombre">
+          <div class="card-img-overlay">
+            <span class="span-overlay">Ropa Hombre</span>
           </div>
-          <div class="p-3">
-            <div class="card text-bg-dark float borde-card" >
-              <img src="./assets/img/imagenes_cartila/ropa_hombre.jpg" class="card-img" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
+
+        </div>
+      </a></div>
+    <div class="p-3"> <a href="#">
+        <div class="card text-bg-dark float borde-card">
+          <img src="./assets/img/imagenes_cartila/ropa_invierno.jpg" class="card-img" alt="Ropa de Invierno">
+          <div class="card-img-overlay">
+            <span class="span-overlay">Coleccion Otoño/Invierno</span>
           </div>
-          <div class="p-3">
-            <div class="card text-bg-dark float borde-card" >
-              <img src="./assets/img/imagenes_cartila/ropa_invierno.jpg" class="card-img" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
+
+        </div>
+      </a></div>
+  </div>
+</section class="conteiner-fluid">
+<section>
+  <div class="d-grid gap-3">
+    <div class="p-3 ">
+      <a href="#">
+        <div class="card text-bg-dark float borde-card">
+          <img src="./assets/img/imagenes_cartila/ver_todo.jpg" class="card-img" alt="Ver Todo">
+          <div class="card-img-overlay">
+            <span class="span-overlay">Ver Todo</span>
           </div>
-          
+        </div>
+      </a>
+    </div>
+</section>
+
+<section class="conteiner-fluid">
+  <h1>Algunos productos</h1>
+  <div class="d-grid gap-3 colum_responsive">
+    <div class="p-3">
+      <div class="card text-bg-dark float borde-card">
+        <img src="./assets/img/imagenes_cartila/ropa_mujer.jpg" class="card-img" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
       </div>
-    </section class="conteiner-fluid">
+    </div>
+    <div class="p-3">
+      <div class="card text-bg-dark float borde-card">
+        <img src="./assets/img/imagenes_cartila/ropa_hombre.jpg" class="card-img" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    <div class="p-3">
+      <div class="card text-bg-dark float borde-card">
+        <img src="./assets/img/imagenes_cartila/ropa_invierno.jpg" class="card-img" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</section class="conteiner-fluid">
